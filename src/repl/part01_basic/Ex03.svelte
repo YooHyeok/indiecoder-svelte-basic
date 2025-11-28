eeba8d98dd5dd213c860e5749a71f950456beac3
유재혁 <yjk7454@naver.com>
2025-11-28 18:20:00 +0900

    feat: `Part01_Svelte 기본문법`: Ex03 - Component 예제

---
+++ src/repl/part01_basic/Ex03.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Header from "./component/ex03/header.svelte";
+  import Content from "./component/ex03/content.svelte";
+  import Footer from "./component/ex03/footer.svelte";
+</script>
+
+<div>
+  <div><h2>Ex03</h2></div>
+  <Header />
+  <Content />
+  <Content />
+  <Content />
+  <Footer />
+</div>
